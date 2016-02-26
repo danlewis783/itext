5aa4e89d999da5179cb1a9630d929b6d4f2976f0
cynic783 <cynic783@gmail.com>
2016-02-26 14:27:03 -0600

    add example of manually converting html to pdf via SAX parsing

---
+++ src/main/java/com/suprtek/HtmlToPdfSaxManual.java
@@ -0,0 +1,198 @@
+package com.suprtek;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import com.lowagie.text.Anchor;
+import com.lowagie.text.Chunk;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Element;
+import com.lowagie.text.Font;
+import com.lowagie.text.List;
+import com.lowagie.text.ListItem;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.TextElementArray;
+import com.lowagie.text.html.HtmlTags;
+import com.lowagie.text.pdf.CMYKColor;
+import com.lowagie.text.pdf.PdfWriter;
+
+// based on Bruno Lowagie examples iText in Action
+public class HtmlToPdfSaxManual extends DefaultHandler {
+    private static final CMYKColor DARK_BLUE = new CMYKColor(0.9f, 0.7f, 0.4f, 0.1f); // RGB=23,69,139
+    private static final Font HELVETICA_DARK_BLUE_DEFAULT_SIZE =
+            new Font(Font.HELVETICA, Font.UNDEFINED, Font.UNDEFINED, DARK_BLUE);
+    private static final int[] HEADING_FONT_SIZES = {24, 18, 16, 14, 12, 10};
+    private static final Map<String, Font> HEADING_FONTS = loadHeadingFonts();
+    private static final String BLOCKQUOTE = "blockquote";
+
+    private Document document;
+    private Stack<Object> stack;
+    private Chunk currentChunk;
+    private String html;
+    private String pdf;
+
+    public HtmlToPdfSaxManual(String html, String pdf) {
+        this.html = html;
+        this.pdf = pdf;
+        document = new Document();
+        stack = new Stack<>();
+    }
+
+    public void start() throws IOException, DocumentException, ParserConfigurationException, SAXException {
+        PdfWriter.getInstance(document, new FileOutputStream(pdf));
+        SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
+        parser.parse(new InputSource(this.getClass().getResourceAsStream(html)), this);
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        String content = new String(ch, start, length);
+        if (content.trim().length() == 0) {
+            return;
+        }
+        if (currentChunk == null) {
+            currentChunk = new Chunk(content.trim());
+        } else {
+            currentChunk.append(" ");
+            currentChunk.append(content.trim());
+        }
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        if (document.isOpen()) {
+            updateStack();
+            for (String heading : HtmlTags.H) {
+                if (heading.equals(qName)) {
+                    flushStack();
+                    Font font = HEADING_FONTS.get(heading);
+                    stack.push(new Paragraph(Float.NaN, "", font));
+                    return;
+                }
+            }
+            if (BLOCKQUOTE.equals(qName)) {
+                flushStack();
+                Paragraph p = new Paragraph();
+                p.setIndentationLeft(50);
+                p.setIndentationRight(20);
+                stack.push(p);
+            } else if (HtmlTags.ANCHOR.equals(qName)) {
+                Anchor anchor = new Anchor("", HELVETICA_DARK_BLUE_DEFAULT_SIZE);
+                anchor.setReference(attributes.getValue(HtmlTags.REFERENCE));
+                stack.push(anchor);
+            } else if (HtmlTags.ORDEREDLIST.equals(qName)) {
+                stack.push(new List(List.ORDERED, 10));
+            } else if (HtmlTags.UNORDEREDLIST.equals(qName)) {
+                stack.push(new List(List.UNORDERED, 10));
+            } else if (HtmlTags.LISTITEM.equals(qName)) {
+                stack.push(new ListItem());
+            } else if (HtmlTags.IMAGE.equals(qName)) {
+                //TODO handle images
+            }
+        } else if (HtmlTags.BODY.equals(qName)) {
+            document.open();
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if (document.isOpen()) {
+            updateStack();
+            for (String heading : HtmlTags.H) {
+                if (heading.equals(qName)) {
+                    flushStack();
+                    return;
+                }
+            }
+            if (BLOCKQUOTE.equals(qName) || HtmlTags.ORDEREDLIST.equals(qName) || HtmlTags.UNORDEREDLIST.equals(qName)) {
+                flushStack();
+            } else if (HtmlTags.NEWLINE.equals(qName)) {
+                currentChunk = Chunk.NEWLINE;
+                updateStack();
+            } else if (HtmlTags.LISTITEM.equals(qName)) {
+                ListItem listItem = (ListItem) stack.pop();
+                List list = (List) stack.pop();
+                list.add(listItem);
+                stack.push(list);
+            } else if (HtmlTags.ANCHOR.equals(qName)) {
+                Anchor anchor = (Anchor) stack.pop();
+                addIfEmptyElseAddToArray(anchor);
+            } else if (HtmlTags.HTML.equals(qName)) {
+                flushStack();
+                document.close();
+            }
+        } else {
+            if (HtmlTags.TITLE.equals(qName)) {
+                document.addTitle(currentChunk.getContent().trim());
+            }
+            currentChunk = null;
+        }
+    }
+
+    private void updateStack() {
+        if (currentChunk != null) {
+            TextElementArray current;
+            if (stack.isEmpty()) {
+                current = new Paragraph();
+            } else {
+                current = (TextElementArray) stack.pop();
+                if (!(current instanceof Paragraph) || !((Paragraph) current).isEmpty()) {
+                    current.add(new Chunk(" "));
+                }
+            }
+            current.add(currentChunk);
+            stack.push(current);
+            currentChunk = null;
+        }
+    }
+
+    private void flushStack() {
+        while (!stack.isEmpty()) {
+            Element element = (Element) stack.pop();
+            addIfEmptyElseAddToArray(element);
+        }
+    }
+
+    private void addIfEmptyElseAddToArray(Element element) {
+        if (stack.isEmpty()) {
+            try {
+                document.add(element);
+            } catch (DocumentException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            TextElementArray prev = (TextElementArray) stack.pop();
+            prev.add(element);
+            stack.push(prev);
+        }
+    }
+
+    private static Map<String, Font> loadHeadingFonts() {
+        Map<String, Font> ret = new HashMap<>();
+        for (int i = 0; i < HtmlTags.H.length; i++) {
+            String tag = HtmlTags.H[i];
+            Font font = new Font(HELVETICA_DARK_BLUE_DEFAULT_SIZE);
+            font.setSize(HEADING_FONT_SIZES[i]);
+            ret.put(tag, font);
+        }
+        return ret;
+    }
+
+    public static void main(final String[] args) throws Exception {
+        HtmlToPdfSaxManual flyer = new HtmlToPdfSaxManual("/htmlToPdfSaxManual.html", "htmlToPdfSaxManual.pdf");
+        flyer.start();
+    }
+}
